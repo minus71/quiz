cd275cf590c5b9ad6ca09ef3a1dc537632ac55ff
Mario Minutolo <mario.minutolo@gmail.com>
2015-05-21 23:06:42 +0200

    Simplified

--- src/quiz/Domino.scala
+++ src/quiz/Domino.scala
@@ -2,23 +2,23 @@ package quiz
 
 case class Token(north: Int, south: Int)
 object Domino {
-  
-  def apply(n: Int): Seq[Token] = {
+
+  def apply(n: Int): List[Token] = {
     if (n <= 1) Nil
     else {
-      val pairs = (1 until n).map(x=>(Token(x, n), Token(n, x)))
-      insert(pairs, apply(n - 1))
+      val cycles:List[Token] = (for (i <- 2 to n - 1) yield (List(Token(n, i), Token(i, n)))).flatten.toList
+      val subseq:List[Token] = Token(1, n) :: cycles ::: Token(n, 1) :: Nil
+      subseq ::: apply(n-1)
     }
   }
- 
+
   def insert(left: Token, right: Token, tokenSequence: Seq[Token]): Seq[Token] = {
     tokenSequence.span { token => token.north != right.south } match {
-      case (lefts, Nil)              => lefts :+ left :+ right
+      case (lefts, Nil)    => lefts :+ left :+ right
       case (lefts, rights) => lefts ++ (left +: right +: rights)
     }
   }
-  
- 
+
   def insert(pairs: Seq[(Token, Token)], tokenSequence: Seq[Token]): Seq[Token] = {
     pairs match {
       case (left, right) +: tail => {
@@ -28,7 +28,7 @@ object Domino {
       case Nil => tokenSequence
     }
   }
- 
+
   def main(args: Array[String]): Unit = {
     (1 to 5).foreach {
       i =>
@@ -38,9 +38,9 @@ object Domino {
         assert(ds.size == ds.toSet.size)
         assert(ds.size == i * (i - 1))
     }
- 
+
   }
- 
+
   def validate(ds: Seq[Token]): Boolean = {
     ds.sliding(2, 1).forall { x =>
       x match {
