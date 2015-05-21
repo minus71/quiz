d6d26670a3dc57da21dff29deedeb1b718584d9c
Mario Minutolo <mario.minutolo@gmail.com>
2015-05-21 23:10:28 +0200

    Tailrec

--- src/quiz/Domino.scala
+++ src/quiz/Domino.scala
@@ -1,32 +1,21 @@
 package quiz
 
+import scala.annotation.tailrec
+
 case class Token(north: Int, south: Int)
 object Domino {
 
   def apply(n: Int): List[Token] = {
-    if (n <= 1) Nil
-    else {
-      val cycles:List[Token] = (for (i <- 2 to n - 1) yield (List(Token(n, i), Token(i, n)))).flatten.toList
-      val subseq:List[Token] = Token(1, n) :: cycles ::: Token(n, 1) :: Nil
-      subseq ::: apply(n-1)
-    }
-  }
-
-  def insert(left: Token, right: Token, tokenSequence: Seq[Token]): Seq[Token] = {
-    tokenSequence.span { token => token.north != right.south } match {
-      case (lefts, Nil)    => lefts :+ left :+ right
-      case (lefts, rights) => lefts ++ (left +: right +: rights)
-    }
-  }
-
-  def insert(pairs: Seq[(Token, Token)], tokenSequence: Seq[Token]): Seq[Token] = {
-    pairs match {
-      case (left, right) +: tail => {
-        def newSeq = insert(left, right, tokenSequence)
-        insert(tail, newSeq)
+    @tailrec
+    def loop(n: Int, acc: List[Token]): List[Token] = {
+      if (n <= 1) acc
+      else {
+        val cycles: List[Token] = (for (i <- 2 to n - 1) yield (List(Token(n, i), Token(i, n)))).flatten.toList
+        val subseq: List[Token] = Token(1, n) :: cycles ::: Token(n, 1) :: Nil
+        loop(n-1, subseq ::: acc)
       }
-      case Nil => tokenSequence
     }
+    loop(n, List())
   }
 
   def main(args: Array[String]): Unit = {
@@ -38,7 +27,6 @@ object Domino {
         assert(ds.size == ds.toSet.size)
         assert(ds.size == i * (i - 1))
     }
-
   }
 
   def validate(ds: Seq[Token]): Boolean = {
